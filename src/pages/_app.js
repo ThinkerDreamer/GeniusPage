cb5f647bee02580c7ec6c62d95bc4578d8a15399
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-25 20:08:27 +0100

    add first login button and custom styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import './App.scss';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { Inter } from '@next/font/google';
+import SSRProvider from 'react-bootstrap/SSRProvider';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ['latin'] });
+
+function App({ Component, pageProps }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      appearance={{
+        variables: {
+          colorPrimary: '#7447D7',
+          colorText: '#271254',
+          fontFamily: inter.style.fontFamily,
+        },
+      }}
+    >
+      <SSRProvider>
+        <Component {...pageProps} />
+      </SSRProvider>
+    </ClerkProvider>
+  );
 }
+
+export default App;
