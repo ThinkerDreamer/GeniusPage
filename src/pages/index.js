8eae5a13b9649ed608356468471717d3fb4c6d24
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-26 12:58:38 +0100

    use logo as favicon, update head title

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>GeniusPage - AI Generated Landing Pages</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
