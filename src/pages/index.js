d3728c84a3e61da74f53ad964a4a9700a92a4467
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-26 14:48:34 +0100

    add navbar and first idea box

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,16 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 import { useClerk } from '@clerk/clerk-react';
-import { Button, Navbar, Container } from 'react-bootstrap';
+import {
+  Button,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Container,
+  Form,
+  FloatingLabel,
+} from 'react-bootstrap';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 // const rightArrow = `&#10132;`;
@@ -13,6 +22,9 @@ const SignUpButton = () => {
 
   return <Button onClick={openSignUp}>Sign up</Button>;
 };
+
+// TODO: Integrate Clerk logged in hook
+const isUserLoggedIn = false;
 
 export default function Home() {
   return (
@@ -27,6 +39,7 @@ export default function Home() {
       </Head>
       <Navbar>
         <Container fluid>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Brand href="#home">
             <img
               src="/logo-dark.svg"
@@ -37,25 +50,80 @@ export default function Home() {
             />{' '}
             GeniusPage
           </Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              {/* If user is logged in, show sites bar, map over user's sites,
+                  if no sites, show create new site button */}
+              {isUserLoggedIn && (
+                <NavDropdown
+                  title="Sites"
+                  id="collasible-nav-dropdown"
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Site #1
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Site #2
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Site #3
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
+            </Nav>
+            <Nav>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <Container fluid>
-        <main className={styles.main}>
-          <div className={styles.description}>
-            <h2>Future Home of GeniusPage</h2>
-            <Image
-              src="/logo-dark.svg"
-              width={150}
-              height={150}
-              priority
-              alt="GeniusPage Logo"
-            ></Image>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <h2>Future Home of GeniusPage</h2>
+          <Image
+            src="/logo-dark.svg"
+            width={150}
+            height={150}
+            priority
+            alt="GeniusPage Logo"
+          ></Image>
+          <p>
+            GeniusPage is an AI-powered landing page builder that
+            helps users quickly and easily create beautiful and
+            effective landing pages for their websites.
+          </p>
+          <p>
+            By leveraging the power of artificial intelligence and
+            machine learning, GeniusPage provides a user-friendly
+            interface that allows users to customize their pages with
+            ease.
+          </p>
+        </div>
+        <Container fluid>
+          <div className={styles.rainbowContainer}>
+            <div className={styles.transparencyOverRainbow}>
+              <Form>
+                <FloatingLabel
+                  controlId="floatingTextarea"
+                  label="Put your startup idea here!"
+                >
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Put your startup idea here!"
+                    style={{ height: '100px' }}
+                  />
+                </FloatingLabel>
+                <p>
+                  Need an idea?{' '}
+                  <a href="#">Here are some examples!</a>
+                </p>
+              </Form>
+              {/* End transparency-over-rainbow */}
+            </div>
           </div>
-
-          {SignUpButton()}
-        </main>
-      </Container>
+        </Container>
+      </main>
     </>
   );
 }
