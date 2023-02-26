ec5f323c23f031e98e8b11230b9c5db93860ac99
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-26 14:48:03 +0100

    use Google font Dosis for all typography

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
 //import '@/styles/globals.css';
 import './App.scss';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { Inter } from '@next/font/google';
+import { Dosis } from '@next/font/google';
 import SSRProvider from 'react-bootstrap/SSRProvider';
 
-const inter = Inter({ subsets: ['latin'] });
+const dosis = Dosis({ subsets: ['latin'] });
 
 function App({ Component, pageProps }) {
   return (
@@ -16,12 +16,14 @@ function App({ Component, pageProps }) {
         variables: {
           colorPrimary: '#7447D7',
           colorText: '#271254',
-          fontFamily: inter.style.fontFamily,
+          fontFamily: dosis.style.fontFamily,
         },
       }}
     >
       <SSRProvider>
-        <Component {...pageProps} />
+        <main className={dosis.className}>
+          <Component {...pageProps} />
+        </main>
       </SSRProvider>
     </ClerkProvider>
   );
