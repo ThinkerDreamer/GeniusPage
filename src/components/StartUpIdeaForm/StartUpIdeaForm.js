51064078477b0f70083993e5e1d413452f557608
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-27 13:05:14 +0100

    move StartUpIdeaForm to new component

---
+++ src/components/StartUpIdeaForm/StartUpIdeaForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, Form, FloatingLabel, Image } from 'react-bootstrap';
+import styles from '@/styles/StartUpIdeaForm.module.css';
+
+function StartUpIdeaForm({ handleGenerateSubmit }) {
+  return (
+    <Form
+      onSubmit={handleGenerateSubmit}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '0 auto',
+      }}
+    >
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Put your startup idea here!"
+      >
+        <Form.Control
+          as="textarea"
+          name="ideaTextArea"
+          placeholder="Put your startup idea here!"
+          style={{ height: '200px', width: '400px' }}
+        />
+      </FloatingLabel>
+      <p>
+        Need an idea? <a href="#">Here are some examples!</a>
+      </p>
+      <Button>
+        <Image
+          src="/lightbulb-white.png"
+          width={30}
+          height={30}
+          className={styles.lightbulbIcon}
+          alt="a brain/lightbulb icon for the Generate button"
+        />{' '}
+        Generate!
+      </Button>
+    </Form>
+  );
+}
+
+export default StartUpIdeaForm;
