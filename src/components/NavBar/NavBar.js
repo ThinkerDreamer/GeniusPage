17102178f2ca79c21d208dff0cca908d8704aab3
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-27 12:47:38 +0100

    create NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+function NavBar({ isUserLoggedIn }) {
+  return (
+    <Navbar>
+      <Container fluid>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo-dark.svg"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="GeniusPage Logo"
+          />{' '}
+          GeniusPage
+        </Navbar.Brand>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {/* If user is logged in, show sites bar, map over user's sites,
+                  if no sites, show create new site button */}
+            {isUserLoggedIn && (
+              <NavDropdown title="Sites" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Site #1
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Site #2
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Site #3
+                </NavDropdown.Item>
+              </NavDropdown>
+            )}
+          </Nav>
+          <Nav>
+            {isUserLoggedIn ? (
+              <Nav.Link href="/sign-out">Logout</Nav.Link>
+            ) : (
+              <Nav.Link href="/sign-in">Login</Nav.Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
