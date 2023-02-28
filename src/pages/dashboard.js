8ad0774a1fcc133415c38da690a2cce343524211
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-02-28 17:43:32 +0100

    create dashboard for logged in, will show sites

---
+++ src/pages/dashboard.js
@@ -0,0 +1,11 @@
+import { Container } from 'react-bootstrap';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      Hey, you're logged in!
+      <br />
+      Here's your dashboard!
+    </Container>
+  );
+}
