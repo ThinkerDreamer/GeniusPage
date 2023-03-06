2ec090caf5d4d4d8ab982d21d63322a5b9885567
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-06 12:14:37 +0100

    add detailed placeholder data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,18 +18,32 @@ export const LandingPageContext = React.createContext();
 
 function App({ Component, pageProps }) {
   const [landingPageData, setLandingPageData] = React.useState({
-    name: 'name',
-    tagline1: 'tagline1',
-    tagline2: 'tagline2',
-    tagline3: 'tagline3',
-    text1: 'text1',
-    text2: 'text2',
-    text3: 'text3',
-    review: 'review',
-    image1: 'public/dog-taxi.png',
+    business_name: 'Your Startup Name',
+    tagline_1: 'We can do it!',
+    advertising_text_1: `Ullamcorper eget nulla facilisi etiam. Aliquet enim tortor
+            at auctor urna nunc id cursus metus. Morbi enim nunc
+            faucibus a pellentesque sit amet. Risus quis varius quam
+            quisque id diam. Vitae suscipit tellus mauris a diam
+            maecenas. Vulputate ut pharetra sit amet aliquam.`,
+    tagline_2: 'Tagline 2 is the best tagline!',
+    advertising_text_2: `Praesent tristique magna sit amet purus gravida quis.
+            Turpis cursus in hac habitasse platea. Ultrices sagittis
+            orci a scelerisque purus semper eget duis at. Odio eu
+            feugiat pretium nibh ipsum consequat nisl vel pretium.
+            Dolor magna eget est lorem ipsum dolor. Mus mauris vitae
+            ultricies leo. Dictum fusce ut placerat orci nulla.`,
+    tagline_3: 'No, tagline 3 is actually the best tagline ever!',
+    advertising_text_3: `Ullamcorper eget nulla facilisi etiam. Aliquet enim tortor
+            at auctor urna nunc id cursus metus. Morbi enim nunc
+            faucibus a pellentesque sit amet. Risus quis varius quam
+            quisque id diam. Vitae suscipit tellus mauris a diam
+            maecenas. Vulputate ut pharetra sit amet aliquam.`,
+    review:
+      'This business is the best one ever! You should give them all your money! --Jane Satisfiedcustomer',
+    image1: `public/dog-taxi.png`,
     image2: 'public/dog-taxi.png',
     image3: 'public/dog-taxi.png',
-    idea: 'idea',
+    idea: 'idea placeholder',
   });
 
   // Pass an object through context, containing both
