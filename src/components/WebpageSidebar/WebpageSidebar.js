d2da8656a3e52ebfd7f939cfc47a3907468ce6b6
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-06 20:08:56 +0100

    Use real, modifiable data in sidebar

--- src/components/WebpageSidebar/WebpageSidebar.js
+++ src/components/WebpageSidebar/WebpageSidebar.js
@@ -6,33 +6,18 @@ function WebpageSidebar() {
   const { landingPageData, setLandingPageData } = React.useContext(
     LandingPageContext
   );
-  const {
-    business_name,
-    tagline_1,
-    tagline_2,
-    tagline_3,
-    advertising_text_1,
-    advertising_text_2,
-    advertising_text_3,
-    review,
-    image1,
-    image2,
-    image3,
-  } = { landingPageData };
 
-  console.log(`lpd in sidebar: ${JSON.stringify(landingPageData)}`);
-  console.log(`lpd keys: ${Object.keys(landingPageData)}`);
-  console.log(
-    `typeof lpd keys: ${typeof Object.keys(landingPageData)}`
-  );
   return (
     <Form>
       <Stack gap={2}>
+        {/* TODO: Rewrite this page mapping over the data.
+          The code below doesn't show anything on the page. */}
+
         {/* {Object.keys(landingPageData).map((key) => {
           <Card key={key}>
             <Card.Header style={{ padding: '0.5rem' }}>
               <Card.Title style={{ marginBottom: 0 }}>
-                {key}
+                {landingPageData[key]}
               </Card.Title>
             </Card.Header>
             <Card.Body>
@@ -53,6 +38,7 @@ function WebpageSidebar() {
             </Card.Body>
           </Card>;
         })} */}
+
         <Card>
           <Card.Header style={{ padding: '0.5rem' }}>
             <Card.Title style={{ marginBottom: 0 }}>Name</Card.Title>
@@ -61,14 +47,14 @@ function WebpageSidebar() {
             <Card.Text>
               <Form.Control
                 type="text"
-                value={business_name}
+                value={landingPageData['business_name']}
                 onChange={(e) => {
                   const nextBusinessName = e.target.value;
                   const nextLandingPageData = {
                     ...landingPageData,
                     business_name: nextBusinessName,
                   };
-                  setUserData(nextLandingPageData);
+                  setLandingPageData(nextLandingPageData);
                 }}
               ></Form.Control>
             </Card.Text>
@@ -78,15 +64,22 @@ function WebpageSidebar() {
         <Card>
           <Card.Header style={{ padding: '0.5rem' }}>
             <Card.Title style={{ marginBottom: 0 }}>
-              Tagline
+              Tagline 1
             </Card.Title>
           </Card.Header>
           <Card.Body>
             <Card.Text>
               <Form.Control
                 type="textArea"
-                value={tagline_1}
-                //onChange={(e) => setStartupTagline(e.target.value)}
+                value={landingPageData['tagline_1']}
+                onChange={(e) => {
+                  const nextTagline1 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    tagline_1: nextTagline1,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
               ></Form.Control>
             </Card.Text>
           </Card.Body>
@@ -94,13 +87,163 @@ function WebpageSidebar() {
 
         <Card>
           <Card.Header style={{ padding: '0.5rem' }}>
-            <Card.Title style={{ marginBottom: 0 }}>Image</Card.Title>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Image 1
+            </Card.Title>
           </Card.Header>
           <Card.Body>
             <Form.Control
               type="file"
               style={{ fontSize: '0.8rem' }}
             />
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Body Text 1
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Control
+                as="textarea"
+                value={landingPageData['advertising_text_1']}
+                onChange={(e) => {
+                  const nextAdText1 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    advertising_text_1: nextAdText1,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
+              ></Form.Control>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Tagline 2
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Control
+                type="textArea"
+                value={landingPageData['tagline_2']}
+                onChange={(e) => {
+                  const nextTagline2 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    tagline_2: nextTagline2,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
+              ></Form.Control>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Image 2
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Form.Control
+              type="file"
+              style={{ fontSize: '0.8rem' }}
+            />
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Body Text 2
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Control
+                as="textarea"
+                value={landingPageData['advertising_text_2']}
+                onChange={(e) => {
+                  const nextAdText2 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    advertising_text_2: nextAdText2,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
+              ></Form.Control>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Tagline 3
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Control
+                type="textArea"
+                value={landingPageData['tagline_3']}
+                onChange={(e) => {
+                  const nextTagline3 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    tagline_3: nextTagline3,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
+              ></Form.Control>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Image 3
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Form.Control
+              type="file"
+              style={{ fontSize: '0.8rem' }}
+            />
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Header style={{ padding: '0.5rem' }}>
+            <Card.Title style={{ marginBottom: 0 }}>
+              Body Text 3
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Form.Control
+                as="textarea"
+                value={landingPageData['advertising_text_3']}
+                onChange={(e) => {
+                  const nextAdText3 = e.target.value;
+                  const nextLandingPageData = {
+                    ...landingPageData,
+                    advertising_text_3: nextAdText3,
+                  };
+                  setLandingPageData(nextLandingPageData);
+                }}
+              ></Form.Control>
+            </Card.Text>
           </Card.Body>
         </Card>
       </Stack>
