2d5ca4eb498e6ea30d77a87c52f2a02ccb2b9cf2
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-06 12:15:20 +0100

    remove unnecessary context

--- src/pages/generatedPage.js
+++ src/pages/generatedPage.js
@@ -10,34 +10,7 @@ import { LandingPageContext } from './_app';
 
 export default function GeneratedPage() {
   const { isSignedIn } = useAuth();
-  const { landingPageData } = React.useContext(LandingPageContext);
-  const startUpData = landingPageData || {
-    business_name: 'Your Startup Name',
-    tagline_1: 'We can do it!',
-    advertising_text_1: `Ullamcorper eget nulla facilisi etiam. Aliquet enim tortor
-            at auctor urna nunc id cursus metus. Morbi enim nunc
-            faucibus a pellentesque sit amet. Risus quis varius quam
-            quisque id diam. Vitae suscipit tellus mauris a diam
-            maecenas. Vulputate ut pharetra sit amet aliquam.`,
-    tagline_2: 'Tagline 2 is the best tagline!',
-    advertising_text_2: `Praesent tristique magna sit amet purus gravida quis.
-            Turpis cursus in hac habitasse platea. Ultrices sagittis
-            orci a scelerisque purus semper eget duis at. Odio eu
-            feugiat pretium nibh ipsum consequat nisl vel pretium.
-            Dolor magna eget est lorem ipsum dolor. Mus mauris vitae
-            ultricies leo. Dictum fusce ut placerat orci nulla.`,
-    tagline_3: 'No, tagline 3 is actually the best tagline ever!',
-    advertising_text_3: `Ullamcorper eget nulla facilisi etiam. Aliquet enim tortor
-            at auctor urna nunc id cursus metus. Morbi enim nunc
-            faucibus a pellentesque sit amet. Risus quis varius quam
-            quisque id diam. Vitae suscipit tellus mauris a diam
-            maecenas. Vulputate ut pharetra sit amet aliquam.`,
-    review:
-      'This business is the best one ever! You should give them all your money! --Jane Satisfiedcustomer',
-    image1: `public/dog-taxi.png`,
-    image2: 'public/dog-taxi.png',
-    image3: 'public/dog-taxi.png',
-  };
+  
   console.log({ startUpData });
   return (
     <>
@@ -59,7 +32,7 @@ export default function GeneratedPage() {
             <WebpageSidebar />
           </Col>
           <Col xs={9}>
-            <WebBrowserWindow startUpData={startUpData} />
+            <WebBrowserWindow />
           </Col>
         </Row>
       </Container>
