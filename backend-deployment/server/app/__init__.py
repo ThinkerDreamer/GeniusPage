5776f91b914dfd3e55a1ea02bd87dbc85ae4a1a4
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2023-03-08 09:36:14 +0100

    comment out stablediffusion calls and dependencies

--- backend-deployment/server/app/__init__.py
+++ backend-deployment/server/app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask import request, jsonify
 from flask_cors import *
-import openai, base64, torch
+import openai#, base64, torch
 import os
 from ..lib.models import User, LandingPage
-from diffusers import StableDiffusionPipeline
-from torch import autocast
-from io import BytesIO
+# from diffusers import StableDiffusionPipeline
+# from torch import autocast
+# from io import BytesIO
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
@@ -86,30 +86,30 @@ def generate_landing_page_infos():
     response['review'] = openai.Completion.create(engine='text-curie-001', prompt=review, max_tokens=25)
     response['review'] = response["review"]['choices'][0]['text']
     
-    devices = "cuda"
-    modelId = "CompVis/stable-diffusion-v1-4"
-    pipe = StableDiffusionPipeline.from_pretrained(modelId, revision="fp16", torch_dtype = torch.float16, use_auth_token = SD_AUTH_TOKEN)
-    pipe.to(devices)
+    # devices = "cuda"
+    # modelId = "CompVis/stable-diffusion-v1-4"
+    # pipe = StableDiffusionPipeline.from_pretrained(modelId, revision="fp16", torch_dtype = torch.float16, use_auth_token = SD_AUTH_TOKEN)
+    # pipe.to(devices)
 
-    with autocast(devices):
-        img = pipe(data, guidance_scale = 8.5).images[0]
-        # img2 = pipe(data, guidance_scale = 8.5).images[0]
-        # img3 = pipe(data, guidance_scale = 8.5).images[0]
+    # with autocast(devices):
+    #     img = pipe(data, guidance_scale = 8.5).images[0]
+    #     img2 = pipe(data, guidance_scale = 8.5).images[0]
+    #     img3 = pipe(data, guidance_scale = 8.5).images[0]
 
-        img.save("resultimage.png")
-        buffer = BytesIO()
-        img.save(buffer, format = "PNG")
-        response['image1'] = base64.b64encode(buffer.getvalue())
+    #     img.save("resultimage.png")
+    #     buffer = BytesIO()
+    #     img.save(buffer, format = "PNG")
+    #     response['image1'] = base64.b64encode(buffer.getvalue())
 
-        # img2.save("result2image.png")
-        # buffer = BytesIO()
-        # img2.save(buffer, format = "PNG")
-        # response['image2'] = base64.b64encode(buffer.getvalue())
+    #     img2.save("result2image.png")
+    #     buffer = BytesIO()
+    #     img2.save(buffer, format = "PNG")
+    #     response['image2'] = base64.b64encode(buffer.getvalue())
 
-        # img3.save("result3image.png")
-        # buffer = BytesIO()
-        # img3.save(buffer, format = "PNG")
-        # response['image3'] = base64.b64encode(buffer.getvalue())
+    #     img3.save("result3image.png")
+    #     buffer = BytesIO()
+    #     img3.save(buffer, format = "PNG")
+    #     response['image3'] = base64.b64encode(buffer.getvalue())
 
 
     response['idea'] = data
